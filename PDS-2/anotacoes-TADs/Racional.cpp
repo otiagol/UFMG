4073591d0fba89f8b85fdac3ef3b9461a2e55c97
Tiago Linhares <tiago.linhares.mello5040@gmail.com>
2026-09-10 21:30:50 -0300

    finalizei racional

--- PDS-2/anotacoes-TADs/Racional.cpp
+++ PDS-2/anotacoes-TADs/Racional.cpp
@@ -1,3 +1,5 @@
+#include "Racional.h"
+
 int Racional::MDC(int a, int b){//como esta separada da class, preciso especificar da onde ela vem
     if(a == 0) return b;
     if (b == 0) return a;
@@ -7,8 +9,8 @@ int Racional::MDC(int a, int b){//como esta separada da class, preciso especific
 // a funcao e inteligente o suficiente pra saber q é pra usar quem esta chamando ela, sendo meio q um parametro indireto, e fazendo o this ser obsoleto
 void Racional::simplificar(){
     if(this->den<0){
-        this->num= -this.num;
-        this->den= -this.den;
+        this->num= -this->num;
+        this->den= -this->den;
     }
     int div= MDC(num, den);//dentro de uma funcao c o msm sobrenome n preciso falar o sobrenome
     this->num= this->num/div;//this significa a variavel q chamou a funcao
@@ -33,11 +35,28 @@ Racional::Racional(int a, int b){
 Racional Racional::operator+(Racional x){
     int nnum= (this->num * x.den)+(this->den * x.num);
     int dden= this->den * x.den;
-    return Racional(nnum, denn);
+    return Racional(nnum, dden);
 }
 Racional Racional::simetrico(){
     return Racional(-this->num, this->den);
 }
 Racional Racional::operator-(Racional x){
     return *this + (x.simetrico());
+}
+Racional Racional::operator*(Racional x){
+    return Racional(num * x.num, den * x.den);
+}
+Racional Racional::inverte(){
+    if(den==0) return *this;
+    return Racional(den, num);
+}
+Racional Racional::operator/(Racional x){
+    return *this * x.inverte();
+}
+bool Racional::operator==(Racional x) {
+    if (num == x.num && den == x.den) {
+        return true; 
+    } else {
+        return false;
+    }
 }
--- PDS-2/anotacoes-TADs/Racional.h
+++ PDS-2/anotacoes-TADs/Racional.h
@@ -1,13 +1,22 @@
-class Racional{//struct eh quando tudo eh publico e class quando tem coisas privadas
+class Racional { // struct eh quando tudo eh publico e class quando tem coisas privadas
 private:
-    int num;//o "_" foi adotado pelos humanos como um jeito de falar q esta no private
+    int num; // o "_" foi adotado pelos humanos como um jeito de falar q esta no private
     int den;
     int MDC(int a, int b);
     void simplificar();
-    Racional();//nasce zerado
-    Racional(int a);//a/1
-    Racional(int a, int b);//a/b
-public:
-    Racional soma(Racional x);
+
+public: // <- O public sobe para cá! Daqui para baixo, tudo é liberado para o usuário
+    Racional(); // nasce zerado
+    Racional(int a); // a/1
+    Racional(int a, int b); // a/b
     
+    Racional operator+(Racional x);
+    Racional operator-(Racional x);
+    Racional operator*(Racional x); // <-- Adicionado: Multiplicação
+    Racional operator/(Racional x); // <-- Adicionado: Divisão
+    
+    Racional simetrico();
+    Racional inverte();             // <-- Adicionado: Inverter a fração
+    
+    bool operator==(Racional x);    // <-- Adicionado: Teste de igualdade
 };
